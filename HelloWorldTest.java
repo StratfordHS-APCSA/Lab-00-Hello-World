cc9269e7c82a586c75c1083a76fc270b996c9ed5
Dave Avis <charles.avis@springbranchisd.com>
2020-09-26 12:05:08 -0500

    Update HelloWorldTest.java

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -1,66 +1,28 @@
-import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-/**
- * The test class HelloWorldTest.
- *
- * @author  Dave Avis
- * @version 06.22.2018
- */
-public class HelloWorldTest
-{
-    private OutputStream os;
-    private PrintStream ps;
-    /**
-     * Default constructor for test class HelloWorldTest.
-     */
-    public HelloWorldTest()
-    {
-    }
+import java.io.*;
 
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
-    @Before
-    public void setUp()
-    {
-        //Prepare to redirect output
-        os = new ByteArrayOutputStream();
-        ps = new PrintStream(os);
-        System.setOut(ps);
-        String ls = System.getProperty("line.separator");
-    }
+public class HelloWorldTest {
 
-    /**
-     * Tests the output of the HelloWorld class.
-     */
-    @Test
-    public void outputTest()
-    {
-        String[] args = null;
-        String expectedOutput = "Hello World!";
-        HelloWorld.main(args);
-        assertEquals("Testing for \"Hello World!\"", expectedOutput, os.toString().trim());
-    }
+   @Test
+   public void testHelloWorld()
+   {
+     PrintStream originalOut = System.out;
+     ByteArrayOutputStream bos = new ByteArrayOutputStream();
+     System.setOut(new PrintStream(bos));
 
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
-    @After
-    public void tearDown()
-    {
-        //Restore normal output
-        PrintStream originalOut = System.out;
-        System.setOut(originalOut);
-    }
+     // action
+     Hello.main(null);
+
+     // assertion
+     assertEquals("Hello world!\n", bos.toString());
+
+     // undo the binding in System
+     System.setOut(originalOut);
+   }
 }
